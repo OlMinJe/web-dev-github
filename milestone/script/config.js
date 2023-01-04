566262bd6a1208ec9377e6f766015c0855d14227
OlMinJe <118544134+OlMinJe@users.noreply.github.com>
2023-01-04 23:07:39 +0900

    lmj
    독기있게 게임 성공

--- milestone/script/config.js
+++ milestone/script/config.js
@@ -1,5 +1,12 @@
-function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
+ function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid; 
+    // html의 데이터 세트 속성을  가져옴 ex. data-nanana
+    // data- 부분은 무시되고, 이후 부분은 객체 속성으로 데이터 집합 객체에 추가됨
+    // 만약 playerid가 아닌 player-id일 경우 dataset['player-id']로 엑세스 해야됨
+
+    // +를 붙여 문자열을 숫자로 변환해준다.
+
+    playerConfigOverlayElement.style.display = 'block'; 
     backdropElement.style.display = 'block';
 }
 
@@ -8,6 +15,7 @@ function closePlayerConfig() {
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event) {
@@ -26,5 +34,16 @@ function savePlayerConfig(event) {
         return; // 값을 반환하면 여기에서 함수의 동작이 멈춘다 -> 에러나면 멈추는 것을 의도함.
     }
 
+    const updataPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updataPlayerDataElement.children[1].textContent = enteredPlayername;
 
-  }
+    //if(editedPlayer === 1) {
+    //    players[0].name = enteredPlayername;
+    //} else {
+    //    players[1].name = enteredPlayername;
+    //}
+
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    closePlayerConfig();
+}
--- milestone/script/game.js
+++ milestone/script/game.js
@@ -1,1 +1,111 @@
- 
+function resetGameStatus() {
+    activePlayer = 0;
+    currentRound = 1;
+    gamelsOver = false;
+    gameOverElement.firstElementChild.innerHTML = 'You won, <span id="winner-name">PLAYER NAME</span>!';
+    gameOverElement.style.display = 'none';
+    
+    let gameBoardIndex = 0;
+
+    for (let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            gameData[i][j] = 0;
+            const gameBoardItemElement = gameBoardElement.children[gameBoardIndex]
+            // children은 배열로 불러옴
+            gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++;
+        }
+    }
+}
+
+function startNewGame() {
+    if (players[0].name === '' && players[1].name === '') {
+        alert('Please set custom player names for both player');
+        return;
+    }
+
+    resetGameStatus();
+
+    gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+    console.log(event.target.tagName);
+    if(event.target.tagName != 'LI' || gamelsOver == true) {
+        return;
+    }
+
+    const selectedField = event.target;
+    // 주의 dataset의 값은 문자열이기 때문에 주의 -> 수학 연산을 통ㅇ해 정수로 바꿈
+    const selectedColum = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+
+    if(gameData[selectedRow][selectedColum] > 0) {
+        alert('Please select an empty field');
+        return;
+    }
+
+    event.target.textContent = players[activePlayer].Symbol; // player[0]부터 시작
+    event.target.classList.add('disabled');
+
+    gameData[selectedRow][selectedColum] = activePlayer + 1
+    console.log(gameData);
+
+    const winnerId = checkForGameOver();
+
+    if (winnerId !== 0) {
+        endGame(winnerId);
+    }
+
+    currentRound++;
+    switchPlayer();
+}
+
+function checkForGameOver() {
+    for (let i = 0; i < 3; i++) {
+        if (gameData[i][0] > 0 && gameData[i][0] === gameData[i][1] && gameData[i][1] === gameData[i][2]) {
+            return gameData[i][0]; // 플레이어의 아이디를 반환
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (gameData[0][i] > 0 && gameData[0][i] === gameData[1][i] && gameData[0][i] === gameData[2][i]) {
+            return gameData[0][i]; // 플레이어의 아이디를 반환
+        }
+    }
+
+    if (gameData[0][0] > 0 && gameData[0][0] === gameData[1][1] && gameData[1][1] === gameData[2][2]) {
+        return gameData[0][0];
+    }
+
+    if (gameData[2][0] > 0 && gameData[2][0] === gameData[1][1] && gameData[1][1] === gameData[0][2]) {
+        return gameData[2][0];
+    }
+
+    if (currentRound === 9) {
+        return -1; // 무승부
+    }
+    return 0;
+}
+
+function endGame(winnerId) {
+      gameOverElement.style.display = 'block';
+
+      if (winnerId > 0) {
+        const winnerName = players[winnerId - 1].name;
+        gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+      } else {
+        gameOverElement.firstElementChild. textContent = 'It\'s a draw!'; 
+      }
+
+      gamelsOver = true;
+}
