3fe4b2431b273f861326f1e9df941bb65aae5667
OlMinJe <118544134+OlMinJe@users.noreply.github.com>
2023-04-06 17:33:10 +0900

    lmj
    <JavaScript>
    - 호이스팅
    - class
    - module
    - 콜백
    - 비동기

---
+++ FrontEnd/03_JAVASCRIPT/module/message.js
@@ -0,0 +1,8 @@
+const message = function() {
+    const name = "박명수";
+    const age = 55;
+
+    return `${name}의 나이는 ${age}세 입니다`
+};
+
+export default message;
---
+++ FrontEnd/03_JAVASCRIPT/module/person.js
@@ -0,0 +1,14 @@
+export const name = "Jesse";
+export const age = 40;
+
+/*
+const name = "Jesse";
+const age = 40;
+
+export {name, age};
+*/
+
+export const text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
